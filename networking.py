77af168fb2b5fa9bb8ecc2d0a5825b44759b484f
Tom Hughes <twhughes91159@gmail.com>
2024-12-09 17:06:05 -0500

    documented DJango section in networking.py

--- networking.py
+++ networking.py
@@ -111,15 +111,14 @@ class RequestHandler(http.server.BaseHTTPRequestHandler):
         self.send_response(200)
 
         # Send headers
-        self.send_header(
-           "Content-type",
-           "text/plain"
-           )
-        
+        self.send_header("Content-type", "text/plain")
+
         self.end_headers()
 
         # Encode and write the response to the client
-        self.wfile.write("\n\t\n\tHello there!\n\t\n\tWelcome to the server!\n\t\n\tThis is a test.".encode())
+        self.wfile.write(
+            "\n\t\n\tHello there!\n\t\n\tWelcome to the server!\n\t\n\tThis is a test.".encode()
+        )
 
 
 # Create an http server object
@@ -129,3 +128,53 @@ server = http.server.HTTPServer(server_address, RequestHandler)
 server.serve_forever()
 
 # To stop the server, press Ctrl + C in the terminal window where the server is running
+
+
+"""
+   DJango Section
+
+      To create a web app with DJango, you will need to follow these steps:
+      
+      1. Install Django using pip
+      
+      2. Create a new Django project using the django-admin command  (django-admin startproject myproject)
+      
+      3. Create a new Django app using the manage.py command (python manage.py startapp myapp)
+      
+      4. Define the models for your app in the models.py file (myapp/models.py)
+      
+      5. Create and run migrations to create the database schema (python manage.py makemigrations, python manage.py migrate)
+      
+      6. Register the models in the admin.py file (myapp/admin.py)
+      
+      7. Create views for your app in the views.py file (myapp/views.py)
+      
+      8. Define the URL patterns in the urls.py file (myapp/urls.py)
+      
+      9. Include the app URL patterns in the project URL patterns (myproject/urls.py)
+      
+      10. Run the development server using the manage.py command (python manage.py runserver)
+"""
+
+# To install Django using pip, run the following command in the terminal
+# pip install django
+
+# To create a new Django project, run the following command in the terminal
+# django-admin startproject myproject
+
+# To create a new Django app, run the following command in the terminal
+# python manage.py startapp myapp
+
+# To create and run migrations, run the following commands in the terminal
+# python manage.py makemigrations
+# python manage.py migrate
+
+# To run the development server, run the following command in the terminal
+# python manage.py runserver
+
+# To stop the development server, press Ctrl + C in the terminal window where the server is running
+
+# To create a superuser for the Django admin interface, run the following command in the terminal
+# python manage.py createsuperuser
+
+# To access the Django admin interface, go to http://localhost:8000/admin in your web browser and log in with the superuser credentials
