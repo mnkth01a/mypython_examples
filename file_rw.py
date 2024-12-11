8541b514915c48c0340d26d1dfb2ce06a21069a2
Tom Hughes <twhughes91159@gmail.com>
2024-12-11 16:42:35 -0500

    playing with files

--- file_rw.py
+++ file_rw.py
@@ -1,17 +1,38 @@
+"""
+    File Open, Read, Write, Append, Remove
+"""
+
+import os
+
+# If the directory doc2 does not exist, create it
+if not os.path.exists("doc2"):
+    os.makedirs("doc2")
+
 ### Set variable fn to the file path ###
-fn = "doc/file_rw.txt"
+fn = "doc2/file_rw.txt"
 
 with open(fn, "w") as file:
     file.write("Hello, World!")  # Write a string to the file
-    
-with open(fn, "r") as file:
-    print(file.read())  # Read the file and print its contents
+
+#with open(fn, "r") as file:
+#    print(file.read())  # Read the file and print its contents
 
 with open(fn, "a") as file:
+    name = input("Enter your name: ")  # Get user input
     file.write("\n")  # Write a newline character to the file
-    file.write("It's me, Tom!")  # Append a string to the file
+    file.write("It's me, " + name)  # Append a string to the file
 
 with open(fn, "r") as file:
-    linefromfile = file.readline()  # Read the first line from the file
+    #linefromfile = file.readline()  # Read the first line from the file
     #print(linefromfile)  # Print the first line with a newline character
-    print(file.read())  # Read the rest of the file and print its contents
+    print(file.read(), '\n')  # Read the rest of the file and print its contents with a newline character
+
+# Remove the file
+os.remove(fn)
+
+# Remove the directory
+import shutil
+
+shutil.rmtree("doc2")
+
+# End of file_rw.py
