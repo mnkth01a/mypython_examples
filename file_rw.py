bba011d9ce385016ad96f3a3e65558b0a6ffd9ae
Tom Hughes <twhughes91159@gmail.com>
2024-12-11 16:46:18 -0500

    ignoring text files

--- file_rw.py
+++ file_rw.py
@@ -14,18 +14,17 @@ fn = "doc2/file_rw.txt"
 with open(fn, "w") as file:
     file.write("Hello, World!")  # Write a string to the file
 
-#with open(fn, "r") as file:
-#    print(file.read())  # Read the file and print its contents
-
 with open(fn, "a") as file:
     name = input("Enter your name: ")  # Get user input
     file.write("\n")  # Write a newline character to the file
     file.write("It's me, " + name)  # Append a string to the file
 
 with open(fn, "r") as file:
-    #linefromfile = file.readline()  # Read the first line from the file
-    #print(linefromfile)  # Print the first line with a newline character
-    print(file.read(), '\n')  # Read the rest of the file and print its contents with a newline character
+    # linefromfile = file.readline()  # Read the first line from the file
+    # print(linefromfile)  # Print the first line with a newline character
+    print(
+        file.read(), "\n"
+    )  # Read the rest of the file and print its contents with a newline character
 
 # Remove the file
 os.remove(fn)
